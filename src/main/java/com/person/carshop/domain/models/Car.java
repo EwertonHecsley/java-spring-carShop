898eefc52a6728c32c40973b197e2645750146b7
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-04-02 11:02:44 -0300

    configuracao docker-compose para database

---
+++ src/main/java/com/person/carshop/domain/models/Car.java
@@ -0,0 +1,11 @@
+package com.person.carshop.domain.models;
+
+public class Car {
+    private Integer id;
+    private String modelo;
+    private Integer ano;
+    private String cor;
+    private String fabricante;
+
+
+}
